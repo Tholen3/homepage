1a26b4f0431be94fa6e78837d9a083ff2ae0d3ba
Hadeon <andersene909@gmail.com>
2016-08-06 12:52:08 -0700

    Character Bio Window, Padding Updates, Navbar Update

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function characterWindow() {
+  var myWindow = window.open("", "myWindow", "width=500,height=300");
+  myWindow.document.write("<body style='background-color:#212830; color:white; font-family:Lato; padding:20px'><div style='text-align:center'><h1 style='margin-top:50px;letter-spacing:2px;font-weight:300'>Character Biographies</h1></br><h2>Hadeon's Character</h2><p>My character name</p><p>Such and such backstory from this region with this history and this is what his objective is going forward. Blah blah blah.</p><h2>Tholen3's Character</h2><p>My character name</p><p>Such and such backstory from this region with this history and this is what his objective is going forward. Blah blah blah.</p></div></body>");
+}

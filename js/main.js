1ea11937c72928524348b58899303fec7ceeed21
Elijah <andersene909@gmail.com>
2016-08-06 16:28:09 -0700

    Mobile Responsive Navbar
    Mobile Responsive Navbar

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ function loreBook() {
   var loreBook = window.open("", "loreBook", "width=600,height=500");
   loreBook.document.write("<link rel='stylesheet' href='stylesheets/styles.css'><body class='paper'><div class='windowBody'><h1>Lorebook</h1></br><h2>Race Overviews</h2><p class='emphasis'>The Pellantine Empire</p><p><ul><li>Comprised of dozens of kingdoms, the Pellantine empire is widely varied.</li><li>The Empire itself is not involved in The Fourth Crusade. The Pellantine navy and army are not present, but rather the religious arm of The Empire is the primary force in the region.</li></ul></p><p class='emphasis'>Nexions</p><p><ul><li>Specializing in necromancy and assassination, the race is well known for their hit-and-run tactics.</li><li>While born with red, orange or yellow eyes, Nexions that are resurrected have their eyes turn to a dull blue glow. Red-eyes see the resurrected as being lower-class due to the fact that they were bested in battle. If a Blue-eye dies, they turn to ash and cannot be brought back again.</li></ul></p></div></body>");
 }
+
+//Hamburger Menu
+$(document).ready(function($) {
+  if ($(window).width() < 750) {
+    $('.container-fluid').hide();
+  } else {
+    $('.mobile-nav').hide();
+  }
+  $('.menu-btn').click(function() {
+    $('.responsive-menu').toggleClass('expand')
+  });
+});
+
+//Screen resize testing
+$(window).resize(function() {
+  if ($(window).width() < 750) {
+    $('.container-fluid').hide();
+    $('.mobile-nav').show();
+  } else {
+
+    $('.mobile-nav').hide();
+    $('.container-fluid').show();
+  }
+});
